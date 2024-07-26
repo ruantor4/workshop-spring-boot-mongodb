63a1b5aa87b5b5dcbb82c4256918030399e84738
Ruan Torquato <ruan.tor4@hotmail.com>
2024-07-26 12:27:57 -0300

    Return users with DTO

--- src/main/java/com/torquato/workshopmongo/controllers/UserController.java
+++ src/main/java/com/torquato/workshopmongo/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.torquato.workshopmongo.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.torquato.workshopmongo.dto.UserDTO;
 import com.torquato.workshopmongo.models.User;
 import com.torquato.workshopmongo.services.UserService;
 
@@ -19,9 +21,10 @@ public class UserController {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
