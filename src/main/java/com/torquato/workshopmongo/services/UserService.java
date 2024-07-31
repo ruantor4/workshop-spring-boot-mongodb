901865fadf910595c63305a29ad39e2cef2cbce0
Ruan Torquato <ruan.tor4@hotmail.com>
2024-07-31 13:12:13 -0300

    Insert User POST

--- src/main/java/com/torquato/workshopmongo/services/UserService.java
+++ src/main/java/com/torquato/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.torquato.workshopmongo.dto.UserDTO;
 import com.torquato.workshopmongo.models.User;
 import com.torquato.workshopmongo.repositories.UserRepository;
 import com.torquato.workshopmongo.services.exception.ObjectNotFoundException;
@@ -25,5 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public User insert (User obj) {
+		return repository.insert(obj);
+	}
 	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
