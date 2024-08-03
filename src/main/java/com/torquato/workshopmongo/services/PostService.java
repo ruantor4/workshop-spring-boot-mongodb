d9d1ce79f8ee17196c4931bd2e0de8bc05423a9c
Ruan Torquato <ruan.tor4@hotmail.com>
2024-08-03 10:17:08 -0300

    Getting a post by id

---
+++ src/main/java/com/torquato/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.torquato.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.torquato.workshopmongo.models.Post;
+import com.torquato.workshopmongo.repositories.PostRepository;
+import com.torquato.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public List<Post> findAll() {
+		return repository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
