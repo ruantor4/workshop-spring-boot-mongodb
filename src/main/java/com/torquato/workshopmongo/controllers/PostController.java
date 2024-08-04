85de9cf40fa049f41f269cb6178aae199ed9f1d3
Ruan Torquato <ruan.tor4@hotmail.com>
2024-08-04 09:04:18 -0300

    Advanced query with @query

--- src/main/java/com/torquato/workshopmongo/controllers/PostController.java
+++ src/main/java/com/torquato/workshopmongo/controllers/PostController.java
@@ -1,5 +1,6 @@
 package com.torquato.workshopmongo.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,18 @@ public class PostController {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/fullsearch" )
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue = "") String text,
+			@RequestParam(value="minDate", defaultValue = "") String minDate,
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/torquato/workshopmongo/services/PostService.java
+++ src/main/java/com/torquato/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.torquato.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,13 +20,18 @@ public class PostService {
 	public List<Post> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Post findById(String id) {
 		Optional<Post> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
-	public List<Post> findByTitle(String text){
+	public List<Post> findByTitle(String text) {
 		return repository.searchTitle(text);
+	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repository.fullSearch(text, minDate, maxDate);
 	}
 }
